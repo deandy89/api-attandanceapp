c7e02c20595d9c0caab540c702a4f3a0767d9c25
deandy89 <deandysyahrial89@gmail.com>
2024-07-24 22:30:09 +0700

    message

--- db.js
+++ db.js
@@ -1,18 +1,35 @@
 const mysql = require('mysql');
-
-const connection = mysql.createConnection({
+const dbConfig = {
   host: '103.247.11.173',
   user: 'adka4379_root',
   password: 'hil@pdeui',
   database: 'adka4379_hris'
-});
+};
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.stack);
-    return;
-  }
-  console.log('Connected to the database as id ' + connection.threadId);
-});
+let connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(dbConfig);
+
+  connection.connect(err => {
+    if (err) {
+      console.error('Error connecting to database:', err);
+      setTimeout(handleDisconnect, 2000); // Reconnect after 2 seconds
+    } else {
+      console.log('Connected to the database');
+    }
+  });
+
+  connection.on('error', err => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.fatal) {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
